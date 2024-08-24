de3a79c2a6d5db2618f24e75abbe6e16a9d37657
MariaEmilia-1 <mariaemiliay99@gmail.com>
2024-08-24 17:38:02 -0300

    update do banner

--- src/pages/Home/Hero.jsx
+++ src/pages/Home/Hero.jsx
@@ -106,6 +106,15 @@ const Hero = () => {
               <p className="text-lg text-gray-600 font-medium">Treinadores profissionais</p>
             </div>
 
+            <div className="flex items-center justify-center flex-col lg:w-fit md:w-[48%]
+            sm:w-[48%] w-full lg:py-6 md:py-7 sm:py-8 py-9 px-10 bg-black/10
+            hover:bg-black/30 rounded-md ease-out duration-700 cursor-pointer">
+              <h5 className="text-4xl mb-2 text-indigo-400 font-semibold uppercase">
+                10+
+              </h5>
+              <p className="text-lg text-gray-600 font-medium">Anos de Funcionamento</p>
+            </div>
+
           </div>
     </div>
     </>
