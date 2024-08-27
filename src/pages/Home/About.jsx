e92316c1b34d99db1df4b38be5c5f925b89300df
EriveltonJr <franciscoerivelton12@gmail.com>
2024-08-27 20:41:00 -0300

    Update no Sobre nós do Site

--- src/pages/Home/About.jsx
+++ src/pages/Home/About.jsx
@@ -1,5 +1,6 @@
 import { Dumbbell } from "lucide-react";
 import React from "react";
+import SecondaryBtn from "../../components/Button/SecondaryBtn";
 
 const About = () => {
   return (
@@ -16,9 +17,23 @@ const About = () => {
       </div>
       <div className ="lg:w-[50%] md:w-full sm:w-full w-full h-auto lg:text-star md:text-star sm:text-star text-center">
         <h6 className="text-lg font-medium text-gray-200 flex items lg:justify-start md:justify-start sm:justify-start justify-center gap-x-2 mb-3">
-          <Dumbbell class name = "w-6 h-6"/>
+          <Dumbbell className = "w-4 h-4 -rotate-45 text-indigo-600"/>
           Sobre nós
         </h6>
+
+        <h1 className="lg:text-3xl md:text-3xl sn:text-2xl text-xk font-semibold
+        text-gray-200 mb-4 leading-normal lg:pe-5 md:pe-4 sm:pe-1 pe-0">
+          Resultados sem esforço, ganhos eficientes: construa seu corpo ideal, mais rápido e com eficácia
+        </h1>
+
+        <p className="lg:text-base md:text-base sm:text-sm text-sm text-gray-500 font-medium mb-4">
+          Transforme sua rotina e alcance seus objetivos de forma rápida e eficiente com o nosso Fitness App. 
+          Projetado para todos os níveis de condicionamento físico, nosso site oferece treinos personalizados, orientações nutricionais e acompanhamento em tempo real para ajudá-lo a construir o corpo que você sempre desejou.
+          Seja você um iniciante ou um atleta experiente, aqui você encontrará as ferramentas e o suporte necessários para atingir seus resultados de forma segura e eficaz.
+          Junte-se à nossa comunidade e comece hoje mesmo a sua jornada para uma vida mais saudável e em forma!
+        </p>
+
+        <SecondaryBtn className="lg:w-auto md:w-auto sm:w-full w-full px-7 py-2 text-base font-medium uppercase justify-center">Torne-se Membro</SecondaryBtn>
       </div>
     </div>
     </>
