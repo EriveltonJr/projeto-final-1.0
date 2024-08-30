766578dabad0239aae5b59c1c735131d7c2eb3a8
EriveltonJr <franciscoerivelton12@gmail.com>
2024-08-30 16:59:08 -0300

    Redirecionamento para seções

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,16 @@
 import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar";
-import Home from "./pages/Home/Home";
+import HomePage from "./pages/Home/Home"; 
 
 function App() {
   return (
-    <>
-      <div className="w-full min-h-screen h-auto bg-[#171717]">
-        <Router>
-          <Navbar />
-          <Home />
-        </Router>
-      </div>
-    </>
+    <div className="w-full min-h-screen h-auto bg-[#171717]">
+      <Router>
+        <Navbar />
+        <HomePage />  
+      </Router>
+    </div>
   );
 }
 
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,16 +3,28 @@ import Hero from "./Hero";
 import About from "./About";
 import IMC from "./IMC";
 
-const Home = () => {
+// Irá Redirecionar para as seções a partir da Navbar
+const HomeSections = () => {  // Novo componente para as seções
   return (
-    <>
-      <div className="w-full h-auto">
-        <Hero/>
-        <About/>
-        <IMC/>
-      </div>
-    </>
+    <div>
+      <section id="hero" className="flex items-center justify-center">
+        <Hero />
+      </section>
+
+      <section id="about" className="flex items-center justify-center">
+        <About />
+      </section>
+
+      <section id="imc" className="flex items-center justify-center">
+        <IMC />
+      </section>
+      
+      {/* Outras Seções */}
+
+    </div>
   );
 };
 
-export default Home;
+export default function Home() {  
+  return <HomeSections />;
+}
