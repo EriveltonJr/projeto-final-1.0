06fd70256570098003ca30713bc918cb2ff475f4
MariaEmilia-1 <mariaemiliay99@gmail.com>
2024-08-30 04:10:03 -0300

    input com o peso e a altura para o calculo IMC

--- src/pages/Home/IMC.jsx
+++ src/pages/Home/IMC.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 import { Dumbbell } from 'lucide-react';
+import Label from '../../components/Label/Label';
+import Input from '../../components/Form/Input';
 
 const IMC = () => {
   const [peso, setPeso] = useState("");
@@ -16,6 +18,32 @@ const IMC = () => {
             <Dumbbell className="w-4 h-4 -rotate-45 text-indigo-600"/>
             Calculadora IMC (Índice de Massa Corporal)
           </h6>
+        </div>
+        <div className="w-full h-auto flex items-end justify-center bg-black/20
+        rounded-md lg:gap-5 md:gap-5 sm:gap-3 gap-2 py-10 lg:px-0 md:px-0 sm:px-2 px-4 
+        flex-wrap">
+          <div className="lg:w-[25%] md:w-[40%] sm:w-[50%] w-full h-auto p-2">
+            <Label htmlfor="peso"> Peso (kg)</Label>
+            <Input 
+              type="text"
+              name="peso" 
+              id="peso" 
+              placeholder="Coloque seu peso"
+              value={peso}
+              onChange={(e) => setPeso(e.target.value)}
+              />
+          </div>
+          <div className="lg:w-[25%] md:w-[40%] sm:w-[50%] w-full h-auto p-2">
+            <Label htmlfor="altura"> Altura (cm)</Label>
+            <Input 
+              type="text"
+              name="Altura" 
+              id="Altura" 
+              placeholder="Coloque sua altura"
+              value={altura}
+              onChange={(e) => setAltura(e.target.value)}
+              />
+          </div>
         </div>
       </div>
     </div>
