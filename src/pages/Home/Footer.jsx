c46e802fd37d985e9c55200cb486e167d0fa7476
EriveltonJr <franciscoerivelton12@gmail.com>
2024-09-13 01:25:14 -0300

    Update Footer - Email, Local e Telefone

--- src/pages/Home/Footer.jsx
+++ src/pages/Home/Footer.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
-import { Dumbbell } from 'lucide-react'
+import { Dumbbell, MailOpen, MapPin, Phone } from 'lucide-react'
 
 const Footer  = () => {
   return (
@@ -19,6 +19,20 @@ const Footer  = () => {
             <p className="text-base text-gray-500 font-normal mb-4 lg:text-start md:text-start sm:text-center text-center">
               Fitness App - Seu guia para uma vida saudável e equilibrada. Acompanhe seu progresso e conquiste seus objetivos com a gente!
             </p>
+            <div className="w-full h-auto space-y-2">
+              <div className="w-full h-auto flex items-center lg:justify-start md:justify-start sm:justify-center justify-center gap-x-2 text-gray-400 hover:text-indigo-600 ease-out duration-300 cursor-pointer">
+                <MapPin className="w-5 h-5" />
+                <p className="text-base">Av Pedro Marques de Medeiros - Parque Industrial, Picos - PI</p>
+              </div>
+              <div className="w-full h-auto flex items-center lg:justify-start md:justify-start sm:justify-center justify-center gap-x-2 text-gray-400 hover:text-indigo-600 ease-out duration-300 cursor-pointer">
+                <MailOpen className="w-5 h-5" />
+                <p className="text-base">IFPI@example.com</p>
+              </div>
+              <div className="w-full h-auto flex items-center lg:justify-start md:justify-start sm:justify-center justify-center gap-x-2 text-gray-400 hover:text-indigo-600 ease-out duration-300 cursor-pointer">
+                <Phone className="w-5 h-5" />
+                <p className="text-base">(89)99999-9999</p>
+              </div>
+            </div>
           </div>
         </div>
       </div>
